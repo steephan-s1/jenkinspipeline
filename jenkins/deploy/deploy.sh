482e346e6839594174f9d22e79cc05c12ab9c7e0
steephan-s1 <58932946+steephan-s1@users.noreply.github.com>
2023-02-13 08:39:57 +0530

    Update deploy.sh

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -5,5 +5,3 @@ echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
 scp -i /opt/prod /tmp/.auth ec2-user@13.235.2.234:/tmp/.auth
-scp -i /opt/prod ./jenkins/deploy/publish ec2-user@13.235.2.234:/tmp/publish
-ssh -i /opt/prod ec2-user@13.235.2.234 "/tmp/publish"
