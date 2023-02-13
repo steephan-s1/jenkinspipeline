c8b86b604537e493d0edec1b84ca1d9753886761
steephan-s1 <58932946+steephan-s1@users.noreply.github.com>
2023-02-13 10:16:58 +0530

    Update deploy.sh

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -4,4 +4,4 @@ echo maven-project > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
-scp -i /opt/prod /tmp/.auth ec2-user@13.235.2.234:/tmp/.auth
+scp -i /opt/prod /tmp/.auth ec2-user@43.205.233.116:/tmp/.auth
