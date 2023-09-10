cbca1d0628eaaa84bc56c624c5fd2142b39d8c37
steephan-s1 <58932946+steephan-s1@users.noreply.github.com>
2023-09-10 10:31:29 +0530

    Update mvn.sh

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -4,6 +4,6 @@ echo "***************************"
 echo "** Building jar ***********"
 echo "***************************"
 
-WORKSPACE=/home/ec2-user/jenkins_home/workspace/demo
+WORKSPACE=/home/ec2-user
 
 docker run --rm  -v  $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
