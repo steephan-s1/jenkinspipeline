0bc71160ea0de37082a125620bcc0c9f4011a16e
steephan-s1 <58932946+steephan-s1@users.noreply.github.com>
2023-10-08 11:22:01 +0530

    Update push.sh

--- jenkins/push/push.sh
+++ jenkins/push/push.sh
@@ -7,7 +7,7 @@ echo "********************"
 IMAGE="maven-project"
 
 echo "** Logging in ***"
-docker login -u haneef786 -p $PASS
+#docker login -u haneef786 -p $PASS
 echo "*** Tagging image ***"
 docker tag $IMAGE:$BUILD_TAG haneef786/$IMAGE:$BUILD_TAG
 echo "*** Pushing image ***"
